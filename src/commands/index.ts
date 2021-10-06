c4c073ce1e810fe05e4b2f07e33f016ceeee8de5
k-kuroguro <riku_2002_1128@i.softbank.jp>
2021-10-06 21:18:00 +0900

    relocate commands file

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,12 +1,12 @@
 import open = require('open');
 import { commands, Disposable, window } from 'vscode';
-import { extensionName } from './constants';
-import { Server } from './server';
+import { extensionName } from '../constants';
+import { Server } from '../server';
 
 const runSimulator = async (path: string) => {
    const sv = new Server(path);
    sv.open();
-   if (await window.showInformationMessage('open http://loclahost:3000 in chrome', 'open') === 'open') {
+   if (await window.showInformationMessage('Open http://loclahost:3000 in chrome', 'Open') === 'Open') {
       open('http://localhost:3000', { app: { name: open.apps.chrome } });
    }
 };
