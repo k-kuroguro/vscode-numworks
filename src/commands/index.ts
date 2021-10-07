ffc617ef06356509b0b9171e030dc7bdc637073d
k-kuroguro <riku_2002_1128@i.softbank.jp>
2021-10-07 12:57:41 +0900

    add command open(Python)Simulator instead of run(Python)Simulator

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -3,18 +3,14 @@ import { commands, Disposable, window } from 'vscode';
 import { extensionName } from '../constants';
 import { Server } from '../server';
 
-const runSimulator = async (sv: Server, pythonOnly: boolean = false) => {
-   sv.open();
-   //TODO: Do not ask again.\
-   //TODO: Open 切り出し
-   const url = `http://localhost:3000${pythonOnly ? '/python' : ''}`;
-   const result = await window.showInformationMessage(`Open ${url} in chrome`, 'Open');
-   if (result === 'Open') open(url, { app: { name: open.apps.chrome } });
+const openSimulator = async (sv: Server, pythonOnly: boolean = false) => {
+   if (!sv.isOpened()) sv.open();
+   open(`http://localhost:3000${pythonOnly ? '/python' : ''}`, { app: { name: open.apps.chrome } });
 };
 
 export const registerCommands = (sv: Server): Disposable[] => {
    return [
-      commands.registerCommand(`${extensionName}.runSimulator`, () => runSimulator(sv)),
-      commands.registerCommand(`${extensionName}.runPythonSimulator`, () => runSimulator(sv, true))
+      commands.registerCommand(`${extensionName}.openSimulator`, () => openSimulator(sv)),
+      commands.registerCommand(`${extensionName}.openPythonSimulator`, () => openSimulator(sv, true))
    ];
 };
