a87806942f7a8a4096b79a18ef9d0c25e5bf9f28
k-kuroguro <riku_2002_1128@i.softbank.jp>
2021-10-12 00:17:42 +0900

    open server when activated extension

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,15 +1,13 @@
 import { commands, Disposable, Uri } from 'vscode';
 import { extensionName } from '../constants';
-import { Server } from '../server';
 import { SimulatorPanel } from '../webview';
 
-const openSimulator = async (extensionUri: Uri, sv: Server) => {
-   if (!sv.isOpened()) sv.open();
+const openSimulator = async (extensionUri: Uri) => {
    SimulatorPanel.createOrShow(extensionUri);
 };
 
-export const registerCommands = (extensionUri: Uri, sv: Server): Disposable[] => {
+export const registerCommands = (extensionUri: Uri): Disposable[] => {
    return [
-      commands.registerCommand(`${extensionName}.openSimulator`, () => openSimulator(extensionUri, sv))
+      commands.registerCommand(`${extensionName}.openSimulator`, () => openSimulator(extensionUri))
    ];
 };
--- src/extension.ts
+++ src/extension.ts
@@ -4,22 +4,17 @@ import { registerCommands } from './commands';
 import { Server } from './server';
 import { Webview } from './webview';
 
-//TODO: server port
-//TODO: setting
-//TODO: activation event
-//TODO: 書き込み 読み込み
-//TODO: リソース整理
-//TODO: webview sv未起動時処理
 //TODO: res minify
 
 export function activate(context: ExtensionContext) {
    //TODO: Uri or Path 統一
    const sv = new Server(path.join(context.extensionPath, 'dist'));
+   sv.open();
 
    context.subscriptions.push(
       sv,
       new Webview(context.extensionUri),
-      ...registerCommands(context.extensionUri, sv)
+      ...registerCommands(context.extensionUri,)
    );
 }
 
