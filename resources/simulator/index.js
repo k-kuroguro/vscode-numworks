3f66cc5d1047c3ad9e1a38d5a563bd538c0da5cc
k-kuroguro <riku_2002_1128@i.softbank.jp>
2021-10-12 12:26:11 +0900

    load scripts into simulator

--- resources/simulator/index.js
+++ resources/simulator/index.js
@@ -10,6 +10,17 @@ if (pythonOnly) {
    module.arguments.push('--code-lock-on-console');
 }
 
+const params = new URLSearchParams(window.location.search);
+const scriptNames = [...params.getAll('scriptName')];
+const scriptContents = [...params.getAll('scriptContent')];
+if (scriptNames.length && scriptContents.length) {
+   module.arguments.push('--code-wipe');
+   for (let i = 0; i < Math.min(scriptNames.length, scriptContents.length); i++) {
+      module.arguments.push('--code-script');
+      module.arguments.push(`${scriptNames[i]}:${scriptContents[i]}`);
+   }
+}
+
 Epsilon(module);
 
 document.querySelectorAll('#keyboard span').forEach(span => {
